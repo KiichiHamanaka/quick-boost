72803bb8bd4c6c510a6b8f47ff8ac8ff67476855
Kiichi Hamanaka <b1695205@planet.kanazawa-it.ac.jp>
2021-11-30 08:46:34 +0900

    リセットCSS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,25 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Provider } from "next-auth/client";
 import Header from "../components/Header";
+import emotionNormalize from "emotion-normalize";
+import { Global, css } from "@emotion/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider session={pageProps.session}>
+      <Global
+        styles={css`
+          ${emotionNormalize}
+          html,
+    body {
+            padding: 0;
+            margin: 0;
+            background: white;
+            min-height: 100%;
+            font-family: Helvetica, Arial, sans-serif;
+          }
+        `}
+      />
       <Header />
       <Component {...pageProps} />
     </Provider>
