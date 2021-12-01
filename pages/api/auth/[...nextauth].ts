021ba8a342494e28525fcc739d5ca3997316e31a
Kiichi Hamanaka <b1695205@planet.kanazawa-it.ac.jp>
2021-12-01 12:27:52 +0900

    認証ログ

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -8,4 +8,15 @@ export default NextAuth({
       clientSecret: process.env.TWITTER_CLIENT_SECRET,
     }),
   ],
+  logger: {
+    error(code, ...message) {
+      console.error(code, message);
+    },
+    warn(code, ...message) {
+      console.warn(code, message);
+    },
+    debug(code, ...message) {
+      console.debug(code, message);
+    },
+  },
 });
