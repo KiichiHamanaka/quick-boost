b863a29bdc927fd9e6917f7221f96eae5f1d42f8
Kiichi Hamanaka <b1695205@planet.kanazawa-it.ac.jp>
2021-12-06 02:55:51 +0900

    remove config

--- config.ts
+++
@@ -1,2 +0,0 @@
-export const masterDB:string = `mongodb+srv://dogages:${process.env.mongoDBpassword}@cluster0.n8qr1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
-export const developDB:string = ``
