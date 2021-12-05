ed4994fb5007123969f44f16605c4094daf5b753
Kiichi Hamanaka <b1695205@planet.kanazawa-it.ac.jp>
2021-12-05 16:14:40 +0900

    session オーバーライド

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,4 +1,4 @@
-// import NextAuth from "next-auth";
+import NextAuth from "next-auth";
 
 declare module "next-auth" {
   /**
@@ -7,6 +7,8 @@ declare module "next-auth" {
   interface Session {
     user: {
       providerID: string;
+      name: string;
+      image: string;
     };
   }
 }
