391a6e3b5684f6fc5ad8b8dc425b48b029955fd4
Kiichi Hamanaka <b1695205@planet.kanazawa-it.ac.jp>
2021-12-07 08:41:15 +0900

    POST,PUTガワ作成

--- pages/api/find/index.ts
+++ pages/api/find/index.ts
@@ -2,9 +2,8 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import Find from "../../../models/Find";
 import connectDB from "../../../lib/atlas";
 
-connectDB();
-
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  await connectDB();
   const {
     query: { id },
     method,
@@ -12,16 +11,24 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   switch (method) {
     case "GET": {
       try {
-        console.log("aaaaaaaaaaa");
         const finds = await Find.find();
         res.status(200).json(finds);
-        console.log(finds);
         break;
       } catch (err) {
+        console.error(err);
         break;
       }
     }
     case "POST": {
+      try {
+        const find = await Find.create(req.body);
+        res.status(200).json(find);
+        break;
+      } catch (e) {
+        break;
+      }
+    }
+    case "PUT": {
       try {
         const find = await Find.create(req.body);
         res.status(200).json(find);
