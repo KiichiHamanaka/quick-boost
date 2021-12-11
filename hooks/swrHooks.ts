36abe39a71a1a8299b8c7ba91db8c881030a1bb9
Kiichi Hamanaka <b1695205@planet.kanazawa-it.ac.jp>
2021-12-11 09:01:02 +0900

    返り値型推論

--- hooks/swrHooks.ts
+++ hooks/swrHooks.ts
@@ -1,8 +1,9 @@
 import useSWR from "swr";
 import * as fetcher from "../lib/fetcher";
 import { User } from "../models/User";
+import { Find } from "../models/Find";
 
-export const useFind = (id: number) => {
+export const useFind = (id: string) => {
   const { data, error } = useSWR(`/api/find/${id}`, fetcher.fetchGet);
 
   return {
@@ -14,9 +15,9 @@ export const useFind = (id: number) => {
 
 export const useFinds = () => {
   const { data, error } = useSWR(`/api/find`, fetcher.fetchGet);
-
+  const res: Array<Find> = data;
   return {
-    finds: data,
+    finds: res,
     isLoading: !error && !data,
     isError: error,
   };
@@ -35,9 +36,17 @@ export const useUser = (id: string) => {
 
 export const useUsers = () => {
   const { data, error } = useSWR(`/api/user`, fetcher.fetchGet);
-
   return {
     users: data,
+    isLoading: !error && !data,
+    isError: error,
+  };
+};
+
+export const useMobileSuits = () => {
+  const { data, error } = useSWR(`/api/MS/`, fetcher.fetchGet);
+  return {
+    mobileSuits: data,
     isLoading: !error && !data,
     isError: error,
   };
