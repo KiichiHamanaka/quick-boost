6ae375a40ac207698cfa924a6504a0edc3d2dedc
Kiichi Hamanaka <b1695205@planet.kanazawa-it.ac.jp>
2022-01-06 01:31:12 +0900

    thread reducer切り出し

--- pages/thread/index.tsx
+++ pages/thread/index.tsx
@@ -1,65 +1,58 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useReducer, useState } from "react";
 import ThreadCard from "../../components/ThreadCard";
 import { useThreads } from "../../hooks/swrHooks";
-import { Thread } from "../../db/models/Thread";
-import { findMobileSuitFromMSID, MobileSuit } from "../../types/MobileSuit";
+import { findMobileSuitFromMSID } from "../../types/MobileSuit";
 import { nonNullable } from "../../types/util";
-
-type option = {
-  startedAt: Date | null;
-  useMS: Array<MobileSuit>;
-  sort: "ASC" | "DESC";
-};
+import SelectMobileSuits from "../../components/SelectMobileSuits";
+import { threadInitialState, threadReducer } from "../../store/thread";
 
 const ThreadIndex: React.FC = () => {
   const { threads, isLoadingThreads, isErrorThreads } = useThreads();
-  const [Threads, setThreads] = useState<Array<Thread>>();
-  const [params, setParams] = useState<option>({
-    startedAt: null,
-    useMS: [],
-    sort: "DESC",
-  }); //ユーザーがフィルタしたいパラメータ
 
-  // useEffect(() => {
-  //   setThreads(
-  //       threads.filter(thread => thread.)
-  //   ); //Threads をフィルタする
-  // }, params);
-  // useEffect(() => {
-  //   setThreads(
-  //       res.filter(thread => thread.)
-  //   ); //Threads をフィルタする
-  // }, params);
-  if (isLoadingThreads) return <div>Loading Animation</div>;
-  if (isErrorThreads) return <div>Error</div>;
+  const [state, dispatch] = useReducer(threadReducer, threadInitialState);
+  const [isShowMSBOX, setIsShowMSBOX] = useState<boolean>(false);
 
-  return (
-    <div>
-      {threads?.map((thread, idx) => {
-        return (
-          <ThreadCard
-            key={idx}
-            ThreadId={thread._id}
-            useMS={
-              thread.useMS &&
-              thread.useMS
-                .map((msid) => msid && findMobileSuitFromMSID(msid))
-                .filter(nonNullable)
-            }
-            playStyle={thread.playStyle}
-            title={thread.title}
-            threadAuthor={thread.threadAuthor}
-            isVC={thread.isVC}
-            startedAt={thread.startedAt}
-            finishedAt={thread.finishedAt}
-            isPlaying={false}
-            position={thread.position}
-            gameMode={thread.gameMode}
-          />
-        );
-      })}
-    </div>
-  );
+  useEffect(() => {
+    dispatch({ type: "fetch", threads: threads });
+  }, [threads]);
+
+  if (isErrorThreads) return <div>なんかおかしいわ</div>;
+  if (isLoadingThreads) {
+    return <div>ロードなう　アニメにせんかい</div>;
+  } else {
+    return (
+      <div>
+        {isShowMSBOX && <SelectMobileSuits dispatch={dispatch} />}
+        <button onClick={() => setIsShowMSBOX(!isShowMSBOX)}>MSから探す</button>
+        {state.threads
+          ? state.threads.map((thread, idx) => {
+              return (
+                <ThreadCard
+                  key={idx}
+                  ThreadId={thread._id}
+                  useMS={
+                    thread.useMS
+                      ? thread.useMS
+                          .map((msid) => findMobileSuitFromMSID(msid))
+                          .filter(nonNullable)
+                      : []
+                  }
+                  playStyle={thread.playStyle}
+                  title={thread.title}
+                  threadAuthor={thread.threadAuthor}
+                  isVC={thread.isVC}
+                  startedAt={thread.startedAt}
+                  finishedAt={thread.finishedAt}
+                  isPlaying={false}
+                  position={thread.position}
+                  gameMode={thread.gameMode}
+                />
+              );
+            })
+          : "なんもない"}
+      </div>
+    );
+  }
 };
 
 export default ThreadIndex;
