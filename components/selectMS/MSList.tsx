e33c8f4e8bbb5f09213ddd795e52582e32985dc1
Kiichi Hamanaka <b1695205@planet.kanazawa-it.ac.jp>
2022-01-08 17:51:36 +0900

    MS選択時CSS

--- components/selectMS/MSList.tsx
+++ components/selectMS/MSList.tsx
@@ -1,12 +1,28 @@
 import Image from "next/image";
 import { MobileSuit, MSImagePath } from "../../types/MobileSuit";
 import React, { Dispatch } from "react";
-import {MSBoxAction} from "../../store/selectMSBox";
+import { MSBoxAction } from "../../store/selectMSBox";
+import { css, SerializedStyles } from "@emotion/react";
 
 type Props = {
   mobileSuits: MobileSuit[];
-  useMS: number[]
+  useMS: number[];
   dispatch: Dispatch<MSBoxAction>;
+};
+
+const FindCardStyle = css`
+  width: 400px;
+  border: solid 1px #2d2d2d;
+  border-radius: 4px;
+  box-shadow: 0 10px 25px 0 rgba(0, 0, 0, 0.5);
+`;
+
+const ChosenStyle = (b: boolean): SerializedStyles => {
+  return b
+    ? css``
+    : css`
+        opacity: 0.5;
+      `;
 };
 
 const MSList = (props: Props) => {
@@ -17,6 +33,7 @@ const MSList = (props: Props) => {
           MS && (
             <div
               key={idx}
+              css={ChosenStyle(props.useMS.includes(MS.id))}
               onClick={() => props.dispatch({ type: "useMS", useMS: MS.id })}
             >
               <Image
