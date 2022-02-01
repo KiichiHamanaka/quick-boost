3b26c522d978f5897721d8e92eb95e1465fa42f5
Kiichi Hamanaka <b1695205@planet.kanazawa-it.ac.jp>
2022-02-01 16:29:19 +0900

    仮辞書

--- db/data/MSDict.ts
+++ db/data/MSDict.ts
@@ -13,6 +13,52 @@ export const MSDict: { [K in number]: MobileSuit } = {
     cost: "2000",
     series: 1,
   },
+  3: {
+    id: 3,
+    name: "シャア専用ゲルググ",
+    cost: "2000",
+    series: 1,
+  },
+  4: {
+    id: 4,
+    name: "ジオング",
+    cost: "2500",
+    series: 1,
+  },
+  5: {
+    id: 5,
+    name: "アッガイ",
+    cost: "1500",
+    series: 1,
+  },
+  6: {
+    id: 6,
+    name: "ギャン",
+    cost: "2000",
+    series: 1,
+  },
+  7: {
+    id: 7,
+    name: "ガンダム(Gメカ)",
+    cost: "2000",
+    series: 1,
+  },
+  8: {
+    id: 8,
+    name: "ザクⅡ(ドアン機)",
+    cost: "1500",
+    series: 1,
+  },
+  9: {
+    id: 9,
+    name: "ガンキャノン",
+    cost: "1500",
+    series: 1,
+  },
+  10: {
+    id: 10,
+    name: "Zガンダム",
+    cost: "2500",
+    series: 2,
+  },
 };
-
-// MSDict[MSID.value]みたいな取得方法
