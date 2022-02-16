140b711ae86f7ca2900f40dad823c4e298ca2375
Kiichi Hamanaka <b1695205@planet.kanazawa-it.ac.jp>
2022-02-16 17:24:41 +0900

    辞書更新

--- db/data/SeriesDict.ts
+++ db/data/SeriesDict.ts
@@ -13,4 +13,28 @@ export const SeriesDict: { [K in number]: Series } = {
     seriesId: 3,
     name: "zz",
   },
+  4: {
+    seriesId: 4,
+    name: "char",
+  },
+  5: {
+    seriesId: 5,
+    name: "f91",
+  },
+  6: {
+    seriesId: 6,
+    name: "v",
+  },
+  7: {
+    seriesId: 7,
+    name: "g",
+  },
+  8: {
+    seriesId: 8,
+    name: "w",
+  },
+  9: {
+    seriesId: 9,
+    name: "w_ew",
+  },
 };
