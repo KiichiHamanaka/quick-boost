5c60fbb7c2f89b7a611cacc68666ec9c114bb5e9
Kiichi Hamanaka <b1695205@planet.kanazawa-it.ac.jp>
2022-02-18 13:58:52 +0900

    inputbox切り出し

--- components/SelectMobileSuits.tsx
+++ components/SelectMobileSuits.tsx
@@ -9,25 +9,12 @@ import React, {
 import { fms, MobileSuit } from "../types/MobileSuit";
 import { useSession } from "next-auth/react";
 import MSList from "./selectMS/MSList";
-import { useThreads } from "../hooks/swrHooks";
 import useSelectMSBox from "../hooks/useSelectMSBox";
-import {
-  Box,
-  Button,
-  FormControl,
-  Input,
-  InputLabel,
-  MenuItem,
-  Select,
-  SelectChangeEvent,
-} from "@mui/material";
-import { Cost } from "../types/Union";
-import { SeriesDict } from "../db/data/SeriesDict";
+import { Input } from "@mui/material";
+import SeriesInputBox from "./selectMS/SeriesInputBox";
+import CostInputBox from "./selectMS/CostInputBox";
 
 export const SelectMobileSuits = () => {
-  // ここにコスト,シリーズ,FavMSフィルタをつける
-
-  const { threadDispatch } = useThreads();
   const { state, mobileSuits, useMS, dispatch } = useSelectMSBox();
   const { data: session, status } = useSession();
   const loading = status === "loading";
@@ -49,89 +36,38 @@ export const SelectMobileSuits = () => {
     }
   }, [session]);
 
-  const handleChange = (event: SelectChangeEvent) => {
-    dispatch({ type: "cost", cost: event.target.value as Cost });
-  };
-
-  const handleChange2 = (event: SelectChangeEvent) => {
-    event.target.value === "ALL"
-      ? dispatch({ type: "seriesId", seriesId: event.target.value })
-      : dispatch({ type: "seriesId", seriesId: parseInt(event.target.value) });
-  };
-
-  const handleChange3 = (
+  const handleChange = (
     event: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
   ) => {
     dispatch({ type: "msName", msName: event.target.value });
   };
 
   const [isFav, setIsFav] = useState<boolean>(false);
-  const [word, setWord] = useState<string | null>(null);
 
   const initialMS = mobileSuits.mobileSuits;
   const [ms, setMS] = useState(initialMS);
 
-  //ダイアログがcloseされたときにinputの選択した値が保持されない
-
   useEffect(() => {
     let result = initialMS;
-    result =
-      state.cost !== "ALL"
-        ? result.filter((m) => m.cost === state.cost)
-        : result;
-    result =
-      state.seriesId !== "ALL"
-        ? result.filter((m) => m.series === state.seriesId)
-        : result;
-    result =
-      state.msName !== ""
-        ? result.filter((m) => m.name.includes(state.msName))
-        : result;
-    setMS(result);
+    if (state.cost !== "ALL") {
+      result = result.filter((m) => m.cost === state.cost);
+    }
+    if (state.seriesId !== "ALL") {
+      result = result.filter((m) => m.series === state.seriesId);
+    }
+    if (state.msName !== "") {
+      result = result.filter((m) => m.name.includes(state.msName));
+    }
     console.log(state);
+    setMS(result);
   }, [state]);
 
   if (loading) return null;
   return (
     <div>
-      <Box sx={{ minWidth: 120 }}>
-        <FormControl fullWidth>
-          <InputLabel id="demo-simple-select-label">シリーズ</InputLabel>
-          <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            label="sid"
-            defaultValue={String(state.seriesId)}
-            onChange={handleChange2}
-          >
-            <MenuItem value={"ALL"}>ALL</MenuItem>
-            {Object.values(SeriesDict).map((s, idx) => (
-              <MenuItem key={idx} value={s.seriesId}>
-                {s.title}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-      </Box>
-      <Box sx={{ minWidth: 120 }}>
-        <FormControl fullWidth>
-          <InputLabel id="demo-simple-select-label">コスト</InputLabel>
-          <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            label="cost"
-            onChange={handleChange}
-            defaultValue={String(state.cost)}
-          >
-            <MenuItem value={"ALL"}>ALL</MenuItem>
-            <MenuItem value={"1500"}>1500</MenuItem>
-            <MenuItem value={"2000"}>2000</MenuItem>
-            <MenuItem value={"2500"}>2500</MenuItem>
-            <MenuItem value={"3000"}>3000</MenuItem>
-          </Select>
-        </FormControl>
-      </Box>
-      <Input onChange={handleChange3} />
+      <SeriesInputBox />
+      <CostInputBox />
+      <Input onChange={handleChange} />
       {!isFav ? (
         <MSList mobileSuits={ms} useMS={useMS} />
       ) : favMS.length ? (
