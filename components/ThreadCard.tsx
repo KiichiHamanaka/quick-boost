a486c3a68cdc81c29163fd8e3be487f23fa4b395
Kiichi Hamanaka <b1695205@planet.kanazawa-it.ac.jp>
2022-02-28 04:33:47 +0900

    クリップボードコピー　アラート

--- components/ThreadCard.tsx
+++ components/ThreadCard.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import { Thread } from "../types/thread/Thread";
 import { findMobileSuitFromMSID, MSImagePath } from "../types/MobileSuit";
 import React from "react";
-import { Box, Typography } from "@mui/material";
+import { Box, Paper, Typography } from "@mui/material";
 
 type ThreadProps = {
   thread: Thread;
@@ -14,48 +14,50 @@ const ThreadCard = (props: ThreadProps) => {
 
   return (
     <Link href={`/thread/${props.thread._id}`} passHref>
-      <Box sx={{ minWidth: "sx", maxWidth: "500px", border: 1 }}>
-        <Typography>{props.thread.title}</Typography>
-        {props.thread.isPlaying ? (
-          <Typography>現在プレイ中！</Typography>
-        ) : (
-          <Typography>現在募集中！</Typography>
-        )}
-        <div>モード：{props.thread.gameMode}</div>
+      <Paper sx={{ minWidth: "sx", maxWidth: "500px", border: 0.5 }}>
         <Box>
-          {!!props.thread.useMS ? (
-            props.thread.useMS.map((ms, idx) => (
-              <Image
-                key={idx}
-                src={MSImagePath(findMobileSuitFromMSID(ms))}
-                alt={findMobileSuitFromMSID(ms).name}
-                loading={"lazy"}
-                width={106}
-                height={52}
-              />
-            ))
+          <Typography>{props.thread.title}</Typography>
+          {props.thread.isPlaying ? (
+            <Typography>現在プレイ中！</Typography>
           ) : (
-            <Typography>Nothing</Typography>
+            <Typography>現在募集中！</Typography>
           )}
+          <div>モード：{props.thread.gameMode}</div>
+          <Box>
+            {!!props.thread.useMS ? (
+              props.thread.useMS.map((ms, idx) => (
+                <Image
+                  key={idx}
+                  src={MSImagePath(findMobileSuitFromMSID(ms))}
+                  alt={findMobileSuitFromMSID(ms).name}
+                  loading={"lazy"}
+                  width={106}
+                  height={52}
+                />
+              ))
+            ) : (
+              <Typography>Nothing</Typography>
+            )}
+          </Box>
+          {props.thread.isVC ? (
+            <Image
+              src={"/assets/Image/Logo/discord.jpeg"}
+              alt={"VC可能"}
+              width={50}
+              height={50}
+            />
+          ) : (
+            <Image
+              src={"/assets/Image/Logo/discord.jpeg"}
+              alt={"VC不可"}
+              width={50}
+              height={50}
+            />
+          )}
+          <Typography>開始日時:{props.thread.startedAt}</Typography>
+          <Typography>終了日時:{props.thread.finishedAt}</Typography>
         </Box>
-        {props.thread.isVC ? (
-          <Image
-            src={"/assets/Image/Logo/discord.jpeg"}
-            alt={"VC可能"}
-            width={50}
-            height={50}
-          />
-        ) : (
-          <Image
-            src={"/assets/Image/Logo/discord.jpeg"}
-            alt={"VC不可"}
-            width={50}
-            height={50}
-          />
-        )}
-        <Typography>開始日時:{props.thread.startedAt}</Typography>
-        <Typography>終了日時:{props.thread.finishedAt}</Typography>
-      </Box>
+      </Paper>
     </Link>
   );
 };
--- pages/thread/[id].tsx
+++ pages/thread/[id].tsx
@@ -1,78 +1,85 @@
 import Image from "next/image";
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
-import { css } from "@emotion/react";
 import { useComments, useThread, useUser } from "../../hooks/swrHooks";
 import { findMobileSuitFromMSID, MSImagePath } from "../../types/MobileSuit";
-import { Box, Typography } from "@mui/material";
-
-//クローズしたスレのURL開いたらそのIDは存在しません処理がいるかも
-
-const FindCardStyle = css`
-  width: 400px;
-  border: solid 1px #2d2d2d;
-  border-radius: 4px;
-  box-shadow: 0 10px 25px 0 rgba(0, 0, 0, 0.5);
-`;
+import { Alert, AlertTitle, Box, Paper, Typography } from "@mui/material";
 
 const ThreadId = () => {
   const router = useRouter();
   const tid = router.query.id as string;
 
   const { thread, isLoadingThread, isErrorThread } = useThread(tid);
-  // const { user, isErrorUser, isLoadingUser } = useUser(thread.threadAuthor);
-  // const { comments, isLoadingComments, isErrorComments } = useComments(tid);
-
+  const [alert, setAlert] = useState(false);
   if (isLoadingThread) return <div>Loading Animation</div>;
   if (isErrorThread) return <div>Error</div>;
 
+  const copyClipboard = () => {
+    navigator.clipboard.writeText(thread.tagCode);
+    setAlert(true);
+    setTimeout(() => setAlert(false), 3000);
+  };
+
   return (
-    <Box sx={{ minWidth: "sx", maxWidth: "500px", border: 1 }}>
-      <Typography>{thread.title}</Typography>
-      {thread.isPlaying ? (
-        <Typography>現在プレイ中！</Typography>
-      ) : (
-        <Typography>現在募集中！</Typography>
+    <div>
+      {alert && (
+        <Alert severity="error">
+          <AlertTitle>コピー完了</AlertTitle>
+          タッグコードをクリップボードにコピーしました！
+        </Alert>
       )}
-      <div>モード：{thread.gameMode}</div>
-      <Box>{thread.body}</Box>
-      <Box>
-        {!!thread.useMS ? (
-          thread.useMS.map((ms, idx) => (
+      <Paper
+        sx={{ minWidth: "sx", border: 0.5 }}
+        onClick={() => copyClipboard()}
+      >
+        <Box>
+          <Typography>{thread.title}</Typography>
+          {thread.isPlaying ? (
+            <Typography>現在プレイ中！</Typography>
+          ) : (
+            <Typography>現在募集中！</Typography>
+          )}
+          <div>モード：{thread.gameMode}</div>
+          <Box>{thread.body}</Box>
+          <Box>
+            {!!thread.useMS ? (
+              thread.useMS.map((ms, idx) => (
+                <Image
+                  key={idx}
+                  src={MSImagePath(findMobileSuitFromMSID(ms))}
+                  alt={findMobileSuitFromMSID(ms).name}
+                  loading={"lazy"}
+                  width={106}
+                  height={52}
+                />
+              ))
+            ) : (
+              <Typography>Nothing</Typography>
+            )}
+          </Box>
+          <Typography>タッグコード:{thread.tagCode}</Typography>
+          <Typography>開始日時:{thread.startedAt}</Typography>
+          <Typography>終了日時:{thread.finishedAt}</Typography>
+          <Typography>作成日:{thread.createdAt}</Typography>
+          {thread.isVC ? (
             <Image
-              key={idx}
-              src={MSImagePath(findMobileSuitFromMSID(ms))}
-              alt={findMobileSuitFromMSID(ms).name}
-              loading={"lazy"}
-              width={106}
-              height={52}
+              src={"/assets/Image/Logo/discord.jpeg"}
+              alt={"VC可能"}
+              width={50}
+              height={50}
             />
-          ))
-        ) : (
-          <Typography>Nothing</Typography>
-        )}
-      </Box>
-      <Typography>タッグコード:{thread.tagCode}</Typography>
-      <Typography>開始日時:{thread.startedAt}</Typography>
-      <Typography>終了日時:{thread.finishedAt}</Typography>
-      <Typography>作成日:{thread.createdAt}</Typography>
-      {thread.isVC ? (
-        <Image
-          src={"/assets/Image/Logo/discord.jpeg"}
-          alt={"VC可能"}
-          width={50}
-          height={50}
-        />
-      ) : (
-        <Image
-          src={"/assets/Image/Logo/discord.jpeg"}
-          alt={"VC不可"}
-          width={50}
-          height={50}
-        />
-      )}
-      コメントエリアのコンポーネント
-    </Box>
+          ) : (
+            <Image
+              src={"/assets/Image/Logo/discord.jpeg"}
+              alt={"VC不可"}
+              width={50}
+              height={50}
+            />
+          )}
+          コメントエリアのコンポーネント
+        </Box>
+      </Paper>
+    </div>
   );
 };
 export default ThreadId;
