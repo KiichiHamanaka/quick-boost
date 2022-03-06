c5eca8536dc3c63567a3a0d97cdfd46035bfcbc8
Kiichi Hamanaka <b1695205@planet.kanazawa-it.ac.jp>
2022-03-07 04:37:40 +0900

    シリーズ辞書更新

--- db/data/SeriesDict.ts
+++ db/data/SeriesDict.ts
@@ -46,4 +46,169 @@ export const SeriesDict: { [K in number]: Series } = {
     name: "w_ew",
     title: "新機動戦記ガンダムW Endless Waltz",
   },
+  10: {
+    seriesId: 10,
+    name: "x",
+    title: "機動新世紀ガンダムX",
+  },
+  11: {
+    seriesId: 11,
+    name: "turn_a",
+    title: "∀ガンダム",
+  },
+  12: {
+    seriesId: 12,
+    name: "seed",
+    title: "機動戦士ガンダムSEED",
+  },
+  13: {
+    seriesId: 13,
+    name: "seed_d",
+    title: "機動戦士ガンダムSEED DESTINY",
+  },
+  14: {
+    seriesId: 14,
+    name: "seed_s",
+    title: "機動戦士ガンダムSEED C.E.73 STARGAZER",
+  },
+  15: {
+    seriesId: 15,
+    name: "00",
+    title: "機動戦士ガンダム00",
+  },
+  16: {
+    seriesId: 16,
+    name: "00_movie",
+    title: "機動戦士ガンダムOO -A wakening of the Trailblazer-",
+  },
+  17: {
+    seriesId: 17,
+    name: "uc",
+    title: "機動戦士ガンダムUC",
+  },
+  18: {
+    seriesId: 18,
+    name: "age",
+    title: "新機動戦記ガンダムAGE",
+  },
+  19: {
+    seriesId: 19,
+    name: "g_reco",
+    title: "Gのレコンギスタ",
+  },
+  20: {
+    seriesId: 20,
+    name: "orphans",
+    title: "機動戦士ガンダム 鉄血のオルフェンズ",
+  },
+  21: {
+    seriesId: 21,
+    name: "nt",
+    title: "機動戦士ガンダムNT",
+  },
+  22: {
+    seriesId: 22,
+    name: "pocket",
+    title: "機動戦士ガンダム0080 ポケットの中の戦争",
+  },
+  23: {
+    seriesId: 23,
+    name: "stardust_memory",
+    title: "機動戦士ガンダム0083 STARDUST MEMORY",
+  },
+  24: {
+    seriesId: 24,
+    name: "the_08th_ms_team",
+    title: "機動戦士ガンダム 第08MS小隊",
+  },
+  25: {
+    seriesId: 25,
+    name: "ms_igloo",
+    title: "機動戦士ガンダム MS IGLOO",
+  },
+  26: {
+    seriesId: 26,
+    name: "thunderbolt",
+    title: "機動戦士ガンダム サンダーボルト",
+  },
+  27: {
+    seriesId: 27,
+    name: "blue_destiny",
+    title: "機動戦士ガンダム外伝 THE BLUE DESTINY",
+  },
+  28: {
+    seriesId: 28,
+    name: "missinglink",
+    title: "機動戦士ガンダム外伝 ミッシングリンク",
+  },
+  29: {
+    seriesId: 29,
+    name: "sentinel",
+    title: "機動戦士ガンダムセンチネル",
+  },
+  30: {
+    seriesId: 30,
+    name: "char_beltorchika",
+    title: "機動戦士ガンダム 逆襲のシャア ベルトーチカ・チルドレン",
+  },
+  31: {
+    seriesId: 31,
+    name: "hathaway",
+    title: "機動戦士ガンダム 閃光のハサウェイ",
+  },
+  32: {
+    seriesId: 32,
+    name: "crossbone",
+    title: "機動戦士クロスボーンガンダム",
+  },
+  33: {
+    seriesId: 33,
+    name: "seed_a",
+    title: "機動戦士ガンダムSEED ASTRAY",
+  },
+  34: {
+    seriesId: 34,
+    name: "valpurgis",
+    title: "機動戦士ガンダムヴァルプルギス",
+  },
+  35: {
+    seriesId: 35,
+    name: "build_f",
+    title: "ガンダムビルドファイターズ",
+  },
+  36: {
+    seriesId: 36,
+    name: "build_t",
+    title: "ガンダムビルドファイターズ トライ",
+  },
+  37: {
+    seriesId: 37,
+    name: "build_ar",
+    title: "ガンダムビルドファイターズ アメイジングレディ",
+  },
+  38: {
+    seriesId: 38,
+    name: "build_d",
+    title: "ガンダムビルドダイバーズ",
+  },
+  39: {
+    seriesId: 39,
+    name: "msv",
+    title: "モビルスーツバリエーション",
+  },
+  40: {
+    seriesId: 40,
+    name: "00_v",
+    title: "機動戦士ガンダム00V",
+  },
+  41: {
+    seriesId: 41,
+    name: "sd",
+    title: "SDガンダム外伝",
+  },
+  42: {
+    seriesId: 42,
+    name: "exa",
+    title: "ガンダムEXA",
+  },
 };
