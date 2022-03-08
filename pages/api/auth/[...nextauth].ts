26eab156451c26918b259bfca0fa76c23f39d024
Kiichi Hamanaka <b1695205@planet.kanazawa-it.ac.jp>
2022-03-08 12:26:03 +0900

    mongoDBAdapter認証追加,初回ログイン時にuser,session,account生成

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,34 +1,48 @@
 import NextAuth from "next-auth";
 import TwitterProvider from "next-auth/providers/twitter";
-import { createUser } from "../create";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import clientPromise from "../../../db/mongodb";
 
 export default NextAuth({
   secret: process.env.SECRET,
+  adapter: MongoDBAdapter(clientPromise),
   providers: [
     TwitterProvider({
       clientId: process.env.TWITTER_CLIENT_ID as string,
       clientSecret: process.env.TWITTER_CLIENT_SECRET as string,
+      // @ts-ignore
+      profile(profile) {
+        return {
+          id: profile.id,
+          name: profile.name,
+          twitterId: profile.screen_name,
+          twitterName: profile.name,
+          openSNSSettings: "No",
+          image: profile.profile_image_url,
+          bio: profile.description,
+        };
+      },
     }),
   ],
 
   callbacks: {
-    async jwt({ token, user, account, profile, isNewUser }) {
-      // isNewUser &&
-      //   createUser({
-      //     twitterUID: profile!.id as number,
-      //     openSNSSettings: "Open",
-      //     twitterId: profile!.screen_name as string,
-      //     twitterName: profile!.name as string,
-      //   });
-      if (profile) {
-        token.screen_name = profile.screen_name;
-        token.twitterUID = profile.id;
-      }
-      return token;
-    },
-    session: async function ({ session, token }) {
-      session.user.screen_name = token.screen_name as string;
-      session.user.twitterUID = token.twitterUID as number;
+    // async jwt({ token, user, profile }) {
+    //   console.log("じょじょじょ");
+    //   if (user) {
+    //     console.log("ゆざざざざ");
+    //     console.log(user);
+    //   }
+    //   if (profile) {
+    //     console.log("ぷろふふふふ");
+    //     console.log(profile);
+    //   }
+    //   console.log("おわりわり");
+    //   return token;
+    // },
+    async session({ session, user }) {
+      session.user.twitterUID = parseInt(user.id);
+      session.user.twitterName = user.twitterName as string;
+      session.user.twitterId = user.twitterId as string;
       return session;
     },
   },
