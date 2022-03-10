558720d53247d60374fbb77ca059a04ce522a784
Kiichi Hamanaka <b1695205@planet.kanazawa-it.ac.jp>
2022-03-10 15:27:03 +0900

    インプットからフォーム用のインプットへ

--- pages/thread/new.tsx
+++ pages/thread/new.tsx
@@ -2,13 +2,13 @@ import React, { useState } from "react";
 import dayjs from "dayjs";
 import timezone from "dayjs/plugin/timezone";
 import utc from "dayjs/plugin/utc";
-import { useForm, SubmitHandler, Controller } from "react-hook-form";
+import { useForm, SubmitHandler } from "react-hook-form";
 import { getSession, useSession } from "next-auth/react";
 import { GameMode, PlayStyle, Position, ThreadStyle } from "../../types/Union";
 import { findMobileSuitFromMSID } from "../../types/MobileSuit";
 import MSDialog from "../../components/dialog/MSSearchDialog";
 import useSelectMSBox from "../../hooks/useSelectMSBox";
-import { Button, TextField, Typography } from "@mui/material";
+import { Button, Typography } from "@mui/material";
 import ShowMSImage from "../../components/selectMS/showMSImager";
 import { createThread } from "../api/create";
 import { GetServerSideProps } from "next";
@@ -18,12 +18,19 @@ import connectDB from "../../db/connectDB";
 import NotSignIn from "../../components/NotSignIn";
 import { useRouter } from "next/router";
 import HookFormInput from "../../components/input/HookFormInput";
+import HookFormInputSelectInput from "../../components/input/HookFormInputSelectInput";
+import {
+  gameMode,
+  playStyle,
+  position,
+  threadStyle,
+} from "../../db/data/FormItems";
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
 dayjs.tz.setDefault("Asia/Tokyo");
 
-type FormValues = {
+export type FormValues = {
   title: string;
   body: string;
   playStyle: PlayStyle;
@@ -42,7 +49,7 @@ type Props = {
 const ThreadNew: React.FC<Props> = ({ fallbackData }) => {
   const router = useRouter();
   const { status } = useSession();
-  const { control, register, handleSubmit } = useForm<FormValues>();
+  const { control, handleSubmit } = useForm<FormValues>();
   const [isShowMSBOX, setIsShowMSBOX] = useState<boolean>(false);
   const { useMS } = useSelectMSBox();
   const loading = status === "loading";
@@ -72,29 +79,59 @@ const ThreadNew: React.FC<Props> = ({ fallbackData }) => {
 
   return (
     <NotSignIn>
-      <Typography>スレッド作成のぺーじ</Typography>
-      <Button onClick={() => setIsShowMSBOX(true)}>MS選択</Button>
       <MSDialog setOpen={setIsShowMSBOX} open={isShowMSBOX} />
       <form onSubmit={handleSubmit(onSubmit)}>
-        スレッド名
-        <HookFormInput name={"title"} control={control} />
-        {/*<input {...register("title")} />*/}
-        本文
-        <input {...register("body")} />
-        プレイスタイル
-        <input {...register("playStyle")} />
-        スレッドスタイル
-        <input {...register("threadStyle")} />
-        ゲームモード
-        <input {...register("gameMode")} />
-        タッグコード
-        <input {...register("tagCode")} />
+        <Typography>スレッド名</Typography>
+        <HookFormInput
+          name={"title"}
+          control={control}
+          placeholder={"スレッド名"}
+        />
+        <Typography>本文</Typography>
+        <HookFormInput name={"body"} control={control} placeholder={"本文"} />
+        <Typography>プレイスタイル</Typography>
+        <HookFormInputSelectInput
+          name={"playStyle"}
+          control={control}
+          menuItem={playStyle}
+          defaultValue={playStyle[0]}
+          helperText={"playStyle"}
+        />
+        <Typography>スレッドスタイル</Typography>
+        <HookFormInputSelectInput
+          name={"threadStyle"}
+          control={control}
+          menuItem={threadStyle}
+          defaultValue={threadStyle[0]}
+          helperText={"threadStyle"}
+        />
+        <Typography>ゲームモード</Typography>
+        <HookFormInputSelectInput
+          name={"gameMode"}
+          control={control}
+          menuItem={gameMode}
+          defaultValue={gameMode[0]}
+          helperText={"gameMode"}
+        />
+        <Typography>タッグコード</Typography>
+        <HookFormInput
+          name={"tagCode"}
+          control={control}
+          placeholder={"タッグコード"}
+        />
+        <Button onClick={() => setIsShowMSBOX(true)}>募集MS選択</Button>
         <ShowMSImage
           MobileSuits={useMS.map((ms) => findMobileSuitFromMSID(ms))}
         />
-        募集立ち回り
-        <input {...register("position")} />
-        <input type="submit" />
+        <Typography>立ち回り</Typography>
+        <HookFormInputSelectInput
+          name={"position"}
+          control={control}
+          menuItem={position}
+          defaultValue={position[0]}
+          helperText={"立ち回り"}
+        />
+        <Button type="submit">送信</Button>
       </form>
     </NotSignIn>
   );
